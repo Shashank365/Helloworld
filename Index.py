ab34a3db6bc72af181d83b4f53bf382d809c9a49
Sai.Shashank <shashank.35641@gmail.com>
2023-11-20 20:49:43 +0530

    Just few lines of code in Helloworld(index.py) file

--- Index.py
+++ Index.py
@@ -6,3 +6,9 @@ a = "Hello, "
 b = input("Enter your name: ")
 c = a + b
 print(c)
+
+# Adding two numbers which are given by the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = a + b
+print("The sum of two number's is: ",c)
