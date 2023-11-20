2698efad7c24574cc5713d82d6656a79898ce66d
Sai.Shashank <shashank.35641@gmail.com>
2023-11-20 21:04:27 +0530

    Created a new branch and few lines of code in index.py

--- Index.py
+++ Index.py
@@ -12,3 +12,11 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = a + b
 print("The sum of two number's is: ",c)
+
+# Telling the user whether his age is above or below 18
+a = input("Enter your name: ")
+b = int(input("Enter your age in number: "))
+if b<18: 
+    print("\"Your age is below 18\"")
+else:
+    print("\"Your age is above 18\"")
