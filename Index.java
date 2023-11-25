29ae9327fa070b36dbac03b25ac7e966374cb1c1
Sai.Shashank <shashank.35641@gmail.com>
2023-11-25 21:19:18 +0530

    Updated the Index.jave file by creating Adding two integers

--- Index.java
+++ Index.java
@@ -3,3 +3,15 @@ class HelloWorld{
         System.out.print("HelloWorld");
     }
 }
+
+class AddingTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println("Adding two Integers");
+        int a = 23;
+        System.out.println("The first integer: "+a);
+        int b = 45;
+        System.out.println("The second integer:"+b);
+        int c = a + b;
+        System.out.println("The sum two integers: "+c);
+    }
+}
